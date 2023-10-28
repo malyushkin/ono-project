cd49b16b8a2aec840779ba7625411427d7f89ffc
Roman Maliushkin <malyushkinr@gmail.com>
2023-10-28 17:53:30 +0200

    Update main.py with exist article

--- main/main.py
+++ main/main.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import re
+import sys
 
 import pandas as pd
 import psycopg2.extras
@@ -10,7 +11,7 @@ from pullenti_analyzer import PullentiAnalyzer
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--postgre-host", default="localhost")
-parser.add_argument("-f", "--file", required=True)
+parser.add_argument("-f", "--file", required=True, help="JSON file name")
 
 args = parser.parse_args()
 args_vars = vars(args)
@@ -29,10 +30,11 @@ POSTGRE_CONFIG = {
 }
 
 # sql queries
-INSERT_ARTICLE_SQL = "INSERT INTO article(article_id, rima_article_id, title, plain_text, published_dt, source_slug) VALUES(%s, %s, %s, %s, %s, %s);"
+SEARCH_ARTICLE_SQL = "SELECT article_id FROM article WHERE rima_article_id={id};"
+INSERT_ARTICLE_SQL = "INSERT INTO article(article_id, rima_article_id, title, plain_text, published_dt, source_slug) VALUES(%s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING;"
 INSERT_ENTITY_SQL = "INSERT INTO entity(entity_id, tag, name) VALUES(%s, %s, %s);"
 INSERT_ENTITY_ATTR_SQL = "INSERT INTO entity_attribute(entity_id, key, value) VALUES(%s, %s, %s);"
-INSERT_ARTICLE_X_ENTITY_SQL = "INSERT INTO article_x_entity(article_id, entity_id) VALUES(%s, %s);"
+INSERT_ARTICLE_X_ENTITY_SQL = "INSERT INTO article_x_entity(article_id, entity_id, is_title) VALUES(%s, %s, %s);"
 
 # db connection
 psycopg2.extras.register_uuid()
@@ -74,22 +76,37 @@ def extend_name(text: str, pattern: str, check_name: str, add_name: str):
     return text
 
 
+def insert_ner(ner_data, is_title=False):
+    for idx, row in ner_data.iterrows():
+        entity_id = uuid4()
+        entity_name = join_str(row) if row.tag == "PERSON" else row["name"]
+
+        entity = (entity_id, row.tag, entity_name)
+        cursor.execute(INSERT_ENTITY_SQL, entity)
+
+        for k, v in row.items():
+            if k not in ["name", "tag"] and type(v) != float:
+                entity_attrs = (entity_id, k, v)
+                cursor.execute(INSERT_ENTITY_ATTR_SQL, entity_attrs)
+
+        article_x_entity = (article_id, entity_id, is_title)
+        cursor.execute(INSERT_ARTICLE_X_ENTITY_SQL, article_x_entity)
+        connection.commit()
+
+
 # read source file
 with open(args_vars["file"]) as f:
     json_data = json.load(f)
 
-for article_item in json_data:
-    try:
-        title = extend_name(article_item["title"], "Навальн", "Алекс", "Алексей")
-        analyzer = PullentiAnalyzer(title, [], PULLENTI_CONFIG)
+for article_item in json_data[:1]:
 
-        ner_data: pd.DataFrame = pd.concat([
-            analyzer.data("GEO"), analyzer.data("ORGANIZATION"), analyzer.data("PERSON")
-        ])
+    cursor.execute(SEARCH_ARTICLE_SQL.format(id=article_item["rima_id"]))
+    search = cursor.fetchone()
 
-        if not ner_data.shape[0]:
-            continue
+    if search:
+        article_id = search[0]
 
+    else:
         article_id = uuid4()
         article = (article_id,
                    article_item["rima_id"],
@@ -98,25 +115,38 @@ for article_item in json_data:
                    article_item["published_dt"],
                    article_item["source_slug"])
         cursor.execute(INSERT_ARTICLE_SQL, article)
-
-        for idx, row in ner_data.iterrows():
-            entity_id = uuid4()
-            entity_name = join_str(row) if row.tag == "PERSON" else row["name"]
-
-            entity = (entity_id, row.tag, entity_name)
-            cursor.execute(INSERT_ENTITY_SQL, entity)
-
-            for k, v in row.items():
-                if k not in ["name", "tag"] and type(v) != float:
-                    entity_attrs = (entity_id, k, v)
-                    cursor.execute(INSERT_ENTITY_ATTR_SQL, entity_attrs)
-
-            article_x_entity = (article_id, entity_id)
-            cursor.execute(INSERT_ARTICLE_X_ENTITY_SQL, article_x_entity)
-
         connection.commit()
+
+    # Title
+    title_process = False
+    if title_process:
+        try:
+            title = extend_name(article_item["title"], "Навальн", "Алекс", "Алексей")
+            analyzer = PullentiAnalyzer(title, [], PULLENTI_CONFIG)
+            ner_data: pd.DataFrame = pd.concat([
+                analyzer.data("GEO"), analyzer.data("ORGANIZATION"), analyzer.data("PERSON")
+            ])
+
+            if ner_data.shape[0]:
+                insert_ner(ner_data, True)
+
+        except Exception as e:
+            print(e)
+            print(article_item["rima_id"], article_item["title"])
+            connection.commit()
+
+    # Content
+    try:
+        content = extend_name(article_item["plain_text"], "Навальн", "Алекс", "Алексей")
+        analyzer = PullentiAnalyzer(content, [], PULLENTI_CONFIG)
+        ner_data: pd.DataFrame = pd.concat([
+            analyzer.data("GEO"), analyzer.data("ORGANIZATION"), analyzer.data("PERSON")
+        ])
+
+        if ner_data.shape[0]:
+            insert_ner(ner_data, False)
 
     except Exception as e:
         print(e)
-        print(article_item["rima_id"], article_item["title"])
+        print(article_item["rima_id"], "...")
         connection.commit()
