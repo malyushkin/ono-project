60777a3f1fee2dc0230ce301820d860171477e93
Roman Maliushkin <malyushkinr@gmail.com>
2023-11-05 13:50:25 +0100

    Add natasha support

---
+++ ner/natasha/README.md
@@ -0,0 +1,3 @@
+1. `python3 -m venv ./venv`
+2. `python3 -m pip install .`
+3. `python3 main.py`
---
+++ ner/natasha/main.py
@@ -0,0 +1,44 @@
+import natasha
+
+from typing import Any, List, Optional, Set, Union
+
+NATASHA_KEYS = {"LOC", "ORG", "PER"}
+
+segmenter = natasha.Segmenter()
+morph_vocab = natasha.MorphVocab()
+emb = natasha.NewsEmbedding()
+morph_tagger = natasha.NewsMorphTagger(emb)
+syntax_parser = natasha.NewsSyntaxParser(emb)
+ner_tagger = natasha.NewsNERTagger(emb)
+names_extractor = natasha.NamesExtractor(morph_vocab)
+
+
+class NatashaClient():
+    def __init__(self, text: str, analyzers: Optional[List[str]] = None):
+        """
+        Args:
+            text:
+            analyzers:
+        Returns:
+            A new cursor object using the connection
+        """
+
+        self.text = text
+        self.analyzers = analyzers if analyzers else NATASHA_KEYS
+
+    def doc(self, text) -> natasha.Doc:
+        doc = natasha.Doc(text)
+        doc.segment(segmenter)
+        doc.tag_morph(morph_tagger)
+        doc.tag_ner(ner_tagger)
+        return doc
+
+    def ner(self) -> Set:
+        doc: natasha.Doc = self.doc(self.text)
+        for span in doc.spans:
+            span.normalize(morph_vocab)
+        return {(_.type, _.normal) for _ in doc.spans}
+
+# text = "Нетаньяху и Путин провели телефонный разговор. По сведениям газеты Haaretz, главной темой беседы стала ситуация вокруг сектора Газа"
+# data = NatashaClient("А " + text)
+# print(data.ner())
