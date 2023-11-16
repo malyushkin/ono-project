6d94d4f97c676f688fc65da49b01369ea9247627
Roman Maliushkin <malyushkinr@gmail.com>
2023-11-16 22:03:32 +0100

    Add tf-large code

---
+++ tf/tf_large/variables.tf
@@ -0,0 +1,28 @@
+variable "aws_key_pair_name" {
+  type    = string
+  default = "id_rsa"
+}
+
+variable "cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "subnet" {
+  default = "10.0.0.0/24"
+}
+
+variable "region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+variable "availability_zone" {
+  type    = string
+  default = "eu-central-1a"
+}
+
+variable "instance_type_main" {
+  type    = string
+  default = "t2.2xlarge"
+}
